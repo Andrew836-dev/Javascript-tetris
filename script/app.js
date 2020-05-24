523b4b27707af8d378b29196c2b997c28650d752
Andrew Gray <andrew.aj.gray@gmail.com>
2020-05-24 12:00:27 +0930

    Tetromino shapes set up, draw function added

--- script/app.js
+++ script/app.js
@@ -6,4 +6,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
+    //The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2, width*2+1],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width+1, width, width*2+1],
+        [width, width+1, width*2+1, width*2+2],
+        [0, width+1, width, width*2+1],
+        [width, width+1, width*2+1, width*2+2]
+    ]
+
+    const tTetromino = [
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1],
+        [1, width, width+1, width+2]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [0, width, width*2, width*3],
+        [width+1, width+2, width+3, width+4],
+        [0, width, width*2, width*3],
+        [width+1, width+2, width+3, width+4]
+    ]
+
+    const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4
+    let current = tetrominoes[4][1]
+
+    // function to draw the shapes
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        });
+    }
+
 })
