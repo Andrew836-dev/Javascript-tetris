86d8d28c24827c1eba34ab668bf503519c35fbac
Andrew Gray <andrew.aj.gray@gmail.com>
2020-05-24 11:26:27 +0930

    Javascript set up

---
+++ script/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Initial setup to find elements in the page
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisply = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+})
